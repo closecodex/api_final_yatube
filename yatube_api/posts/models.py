2c7a485151f139a95a082748b74e86b5e1c76954
closecodex <144854827+closecodex@users.noreply.github.com>
2024-07-21 02:13:42 +0100

    Update models.py

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 User = get_user_model()
 
+
 class Group(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
@@ -19,8 +20,7 @@ class Post(models.Model):
         User, on_delete=models.CASCADE, related_name='posts')
     group = models.ForeignKey(
         Group, on_delete=models.SET_NULL, related_name='posts',
-        null=True, blank=True
-        )
+        null=True, blank=True)
     image = models.ImageField(
         upload_to='posts/', null=True, blank=True)
 
@@ -36,7 +36,7 @@ class Comment(models.Model):
     text = models.TextField()
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
-    
+
     def __str__(self):
         return self.text
 
@@ -46,7 +46,7 @@ class Follow(models.Model):
         User, on_delete=models.CASCADE, related_name='followers')
     following = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='following')
-    
+
     class Meta:
         constraints = [
             models.UniqueConstraint(
