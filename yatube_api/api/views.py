8be54dd3c6ebdbc66c4ebbd28732fc49cccfc1da
closecodex <144854827+closecodex@users.noreply.github.com>
2024-07-21 23:27:38 +0100

    Update views.py

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -2,20 +2,18 @@ from http import HTTPStatus
 
 from django.shortcuts import get_object_or_404
 
-from rest_framework import viewsets, permissions, filters
+from rest_framework import viewsets, permissions, filters, mixins
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.response import Response
-from rest_framework.exceptions import ValidationError
 
-from api.serializers import (PostSerializer, GroupSerializer,
+from api.serializers import (PostSerializer, GroupSerializer, 
                              CommentSerializer, FollowSerializer)
-from posts.models import Post, Group, Follow, Comment
-
+from posts.models import Post, Group, Follow
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     pagination_class = LimitOffsetPagination
 
     def perform_create(self, serializer):
@@ -34,30 +32,25 @@ class PostViewSet(viewsets.ModelViewSet):
         return super().destroy(request, *args, **kwargs)
 
     def get_permissions(self):
-        if self.action in ['list', 'retrieve']:
-            self.permission_classes = [permissions.AllowAny]
-        else:
-            self.permission_classes = [permissions.IsAuthenticated]
-        return super().get_permissions()
+        if self.action in ('list', 'retrieve'):
+            return [permissions.AllowAny()]
+        return [permissions.IsAuthenticated()]
 
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = (permissions.AllowAny,)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
     def get_queryset(self):
-        post = self.get_post()
-        return post.comments.all()
+        return self.get_post().comments.all()
 
     def perform_create(self, serializer):
-        post = self.get_post()
-        serializer.save(author=self.request.user, post=post)
+        serializer.save(author=self.request.user, post=self.get_post())
 
     def update(self, request, *args, **kwargs):
         comment = self.get_object()
@@ -75,28 +68,22 @@ class CommentViewSet(viewsets.ModelViewSet):
         return get_object_or_404(Post, pk=self.kwargs.get('post_id'))
 
     def get_permissions(self):
-        if self.action in ['list', 'retrieve']:
-            self.permission_classes = [permissions.AllowAny]
-        else:
-            self.permission_classes = [permissions.IsAuthenticated]
-        return super().get_permissions()
+        if self.action in ('list', 'retrieve'):
+            return [permissions.AllowAny()]
+        return [permissions.IsAuthenticated()]
 
 
-class FollowViewSet(viewsets.ModelViewSet):
+class FollowViewSet(mixins.CreateModelMixin,
+                    mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
     queryset = Follow.objects.all()
     serializer_class = FollowSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = (permissions.IsAuthenticated,)
     filter_backends = [filters.SearchFilter]
     search_fields = ['following__username']
 
     def get_queryset(self):
-        user = self.request.user
-        queryset = Follow.objects.filter(user=user)
-        return queryset.all()
+        return Follow.objects.filter(user=self.request.user).all()
 
     def perform_create(self, serializer):
-        following = serializer.validated_data.get('following')
-        user = self.request.user
-        if Follow.objects.filter(user=user, following=following).exists():
-            raise ValidationError("Вы уже подписаны на этого пользователя.")
-        serializer.save(user=user)
+        serializer.save(user=self.request.user)
