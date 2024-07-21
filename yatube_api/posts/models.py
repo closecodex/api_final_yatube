32a2775e09a445613e62cd9d88c2717137221ce4
closecodex <144854827+closecodex@users.noreply.github.com>
2024-07-21 23:28:05 +0100

    Update models.py

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -3,26 +3,42 @@ from django.db import models
 
 User = get_user_model()
 
+STR_PREVIEW_LENGTH = 50
+
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    description = models.TextField()
+    title = models.CharField(max_length=200, verbose_name='Название группы')
+    slug = models.SlugField(unique=True, verbose_name='URL группы')
+    description = models.TextField(verbose_name='Описание группы')
+
+    class Meta:
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
 
     def __str__(self):
         return self.title
 
 
 class Post(models.Model):
-    text = models.TextField()
+    text = models.TextField(verbose_name='Текст поста')
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts')
+        User, on_delete=models.CASCADE, related_name='posts',
+        verbose_name='Автор'
+    )
     group = models.ForeignKey(
-        Group, on_delete=models.SET_NULL, related_name='posts',
-        null=True, blank=True)
+        Group, on_delete=models.SET_NULL,
+        related_name='posts', blank=True, null=True,
+        verbose_name='Группа'
+    )
     image = models.ImageField(
-        upload_to='posts/', null=True, blank=True)
+        upload_to='posts/', blank=True,
+        verbose_name='Изображение'
+    )
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
 
     def __str__(self):
         return self.text
@@ -30,27 +46,46 @@ class Post(models.Model):
 
 class Comment(models.Model):
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
+        User, on_delete=models.CASCADE, related_name='comments',
+        verbose_name='Автор'
+    )
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments')
-    text = models.TextField()
+        Post, on_delete=models.CASCADE, related_name='comments',
+        verbose_name='Пост'
+    )
+    text = models.TextField(verbose_name='Текст комментария')
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
+    
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
 
     def __str__(self):
-        return self.text
+        return self.text[:STR_PREVIEW_LENGTH]
 
 
 class Follow(models.Model):
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='followers')
+        User, on_delete=models.CASCADE, related_name='followers',
+        verbose_name='Пользователь'
+    )
     following = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='following')
-
+        User, on_delete=models.CASCADE, related_name='following',
+        verbose_name='Подписка'
+    )
+    
     class Meta:
-        constraints = [
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+        constraints = (
             models.UniqueConstraint(
-                fields=['user', 'following'],
-                name='user_following'
-            )
-        ]
+                fields=('user', 'following'),
+                name='unique_follow'
+            ),
+        )
+
+    def save(self, *args, **kwargs):
+        if self.user == self.following:
+            raise ValueError("Вы не можете подписаться на самого себя!")
+        super().save(*args, **kwargs)
